b1dd89241a387c5f70fe3922280707044cec506b
Antoine Ancelin <ancelin.ant@gmail.com>
2026-05-10 22:30:41 +0200

    fix(types): disable ESLint namespace rules for Express augmentation

--- src/types/express.ts
+++ src/types/express.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 declare global {
   namespace Express {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface Request {
       // user?: { id: string; email: string }; // uncomment when adding auth middleware
     }
